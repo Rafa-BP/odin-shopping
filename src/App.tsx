4b628658ffa23c578653ef3d25334cf02a4e0f0e
Rafa-BP <rafael.bolfe.pasqualotto@gmail.com>
2026-03-09 16:33:00 -0300

    Made product fetching logic.

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,39 @@ import "./tailwind.css";
 import { Outlet } from "react-router";
 
 import NavButton from "./components/NavButton";
+import Footer from "./components/Footer";
+import { useEffect, useState } from "react";
+
+interface product {
+  id: number,
+  title: string,
+  price: number,
+  description: string,
+  category: string,
+  image: string
+};
 
 function App() {
+
+  const [products, setProducts] = useState<product[]>([]);
+
+  useEffect(() => {
+    async function getData() {
+      const response = await fetch('https://fakestoreapi.com/products');
+      if (response.status != 200) {
+        console.log("error");
+      }
+      const data = await response.json();
+
+      setProducts(data);
+    }
+  getData();
+
+  }, []);
+
   return (
     <>
-      <nav className="flex py-6 px-16 w-full items-center justify-between bg-gray-600 shadow-lg">
+      <nav className="flex py-6 px-16 w-full items-center justify-between bg-gray-800 shadow-lg">
         <h1 className="text-4xl text-white">Fake Shop</h1>
         <div className="flex gap-4">
           <NavButton text="Home" link="home" />
@@ -15,6 +43,7 @@ function App() {
         </div>
       </nav>
       <Outlet />
+      <Footer />
     </>
   );
 }
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -2,9 +2,9 @@ import "../tailwind.css";
 
 function Home() {
   return (
-    <>
-      <h1>Huuhh test</h1>
-    </>
+    <main className="bg-gray-700 w-full h-dvh flex justify-center items-center">
+      <h1 className="text-white text-xl">Testing</h1>
+    </main>
   );
 }
 
